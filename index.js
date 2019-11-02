d8ab9a6c928ccddc79df290f93af809fa6f7424e
Parker Thompson <park.thompson@gmail.com>
2019-11-02 15:55:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let newArray = [];
+  for (var i = 0; i < musicians.length; i++) {
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newArray;
+}
+
+function johnLennonFacts(facts) {
+  let i = 0;
+  let newFacts = [];
+  while (i < facts.length) {
+    newFacts[i] = `${facts[i]}!!!`
+    i++
+  }
+  return newFacts;
+}
+
+function iLoveTheBeatles(num) {
+  let arr = [];
+  do {
+    arr.push('I love the Beatles!');
+    num++;
+  } while (num < 15)
+  return arr;
+}
