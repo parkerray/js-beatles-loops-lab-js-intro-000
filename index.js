df5715b872344dd6413a6a7de26d560b01ba8235
Parker Thompson <park.thompson@gmail.com>
2019-11-02 15:44:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  let array = [];
+  let newArray = [];
+  for (var i = 0; i < musicians.length; i++) {
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newArray;
 }
